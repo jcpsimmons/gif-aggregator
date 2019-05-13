fa69863bf63dc437f2e41135e367451a4c142beb
simsies <jcpsimmons@gmail.com>
2019-05-13 16:20:06 -0700

    folder restruct.

--- index.js
+++ index.js
@@ -48,8 +48,8 @@ app.post("/api/v1/search", function(req, res) {
     });
 });
 
-const PORT = 5000;
+const port = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(port, () => {
   console.log(`Server running on port ${PORT}`);
 });
